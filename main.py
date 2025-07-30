b599501ddbd07b8c3983f951ae89529c9ee93602
yashrampersad <yash.rampersad.2007@gmail.com>
2025-07-30 19:27:49 +0100

    basic pygame window

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print("yash bash")
+import pygame
 
-print("new")
+pygame.init()
+
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
+
+colours = {"WHITE":"#d9d9d9",
+           "LIGHT SILVER":"#717d9f",
+           "DARK SILVER":"#667192",
+           "GREY":"#4e5770",
+           "LIGHT COAL":"#353a49",
+           "DARK COAL":"#2c2f3b",}
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Minespeeder")
+
+# game loop
+while True:
+
+    screen.fill(colours["GREY"])
+    
+    # event handler for actions like mouse clicks
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    
+    pygame.display.update()
