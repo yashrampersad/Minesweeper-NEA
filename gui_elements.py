bbb1e9ff5b1064424ea73bf4135462a1a1b73d57
yashrampersad <yash.rampersad.2007@gmail.com>
2025-08-10 17:46:46 +0100

    core elements fully debugged

--- gui_elements.py
+++ gui_elements.py
@@ -6,8 +6,8 @@ pygame.init()
 FONT_SIZE = 40
 LABEL_HEIGHT = 50
 PROGRESS_BAR_HEIGHT = 50
-CORNER_ROUNDING = 15
-TEXT_PADDING = 20
+CORNER_ROUNDING = 40
+TEXT_PADDING = 25
 
 main_font = pygame.font.SysFont("couriernew", FONT_SIZE, bold=True) # set font and text size
 number_font = pygame.font.SysFont("arial", FONT_SIZE, bold=True)
@@ -20,8 +20,8 @@ class Label(): # for displaying text
 
     def draw(self, surface, x, y):
         text = main_font.render(self.text, True, self.text_colour) # render the text in the correct font and style
-        pygame.draw.rect(surface, self.colour, (x, y, text.get_width()+TEXT_PADDING, LABEL_HEIGHT), border_radius=CORNER_ROUNDING) # draw the box the same width as the text with some padding
-        surface.blit(self.text, (x,y))
+        pygame.draw.rect(surface, self.colour, (x, y, text.get_width()+TEXT_PADDING*2, text.get_height()+TEXT_PADDING*2), border_radius=CORNER_ROUNDING) # draw the box the same width as the text with some padding
+        surface.blit(text, (x+TEXT_PADDING,y+TEXT_PADDING))
 
 class Button(Label): # for clickable buttons that display text
     def __init__(self, colour, hover_colour, text, text_colour):
@@ -33,13 +33,13 @@ class Button(Label): # for clickable buttons that display text
     # using polymorphism to change the draw method to define a hitbox we can check for collisions with
     def draw(self, surface, x, y):
         text = main_font.render(self.text, True, self.text_colour)
-        self.hitbox = pygame.Rect(x, y, text.get_width()+TEXT_PADDING, LABEL_HEIGHT)
+        self.hitbox = pygame.Rect(x, y, text.get_width()+TEXT_PADDING*2, text.get_height()+TEXT_PADDING*2)
         # change colour if the cursor is hovering over
         if self.highlighted:
             pygame.draw.rect(surface, self.hover_colour, self.hitbox, border_radius=CORNER_ROUNDING)
         else:
-            pygame.draw.rect(surface, self.hover_colour, self.hitbox, border_radius=CORNER_ROUNDING)
-        surface.blit(text, (x,y))
+            pygame.draw.rect(surface, self.colour, self.hitbox, border_radius=CORNER_ROUNDING)
+        surface.blit(text, (x+TEXT_PADDING,y+TEXT_PADDING))
     
     def isClicked(self, event):
         mouse_pos = pygame.mouse.get_pos()
@@ -52,21 +52,95 @@ class Button(Label): # for clickable buttons that display text
                 return False
         else:
             self.highlighted = False
+            return False
 
 class StateButton(Button): # for buttons that cycle through different values eg, beginner, intermediate, expert, custom
     def __init__(self, colour, hover_colour, states, text_colour):
         self.current_state = 0
-        super().__init__(self, colour, hover_colour, states[self.current_state], text_colour)
+        super().__init__(colour, hover_colour, states[self.current_state], text_colour)
         self.states = states
         # the states are in a given array, and self.current_state points to the current state
 
-    def updateState(self):
-        if super().isClicked(): # the StateButton runs the method it inherited from Button to check for clicks
+    def updateState(self, event):
+        if super().isClicked(event): # the StateButton runs the method it inherited from Button to check for clicks
             self.current_state = (self.current_state + 1) % len(self.states)
             self.text = self.states[self.current_state]
         return self.states[self.current_state]
 
-class UISquare(): # a single square on the minesweeper board to detect clicks for the mineswepper section
+class InputBox(Button): # for the user to input text
+    def __init__(self, colour, hover_colour, default_text, text_colour):
+        self.default_text = default_text
+        self.current_text = ""
+        self.activated = False
+        super().__init__(colour, hover_colour, default_text, text_colour)
+
+    def draw(self, surface, x, y):
+        text = main_font.render(self.text, True, self.text_colour)
+        default_text = main_font.render(self.default_text, True, self.text_colour)
+        if text.get_width() < default_text.get_width():
+            width = default_text.get_width()
+        else:
+            width = text.get_width()
+        self.hitbox = pygame.Rect(x, y, width+TEXT_PADDING*2, text.get_height()+TEXT_PADDING*2)
+        # change colour if the cursor is hovering over
+        if self.highlighted:
+            pygame.draw.rect(surface, self.hover_colour, self.hitbox, border_radius=CORNER_ROUNDING)
+        else:
+            pygame.draw.rect(surface, self.colour, self.hitbox, border_radius=CORNER_ROUNDING)
+        surface.blit(text, (x+TEXT_PADDING,y+TEXT_PADDING))
+
+    def update(self, event):
+        mouse_pos = pygame.mouse.get_pos()
+        
+        # activated represents the state where the user can edit the text in the InputBox
+        if not self.activated:
+            self.highlighted = False
+            # once the user clicks on the box it becomes activated and they can enter text
+            if self.hitbox.collidepoint(mouse_pos) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                self.activated = True
+
+        elif self.activated:
+            self.highlighted = True
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    self.current_text = self.current_text[:-1]
+                # if they click enter, return the text
+                elif event.key == pygame.K_RETURN:
+                    self.activated = False
+                    self.highlighted = False
+                    return self.current_text
+                else:
+                    if event.key not in [pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_LCTRL, pygame.K_RCTRL]:
+                        self.current_text += event.unicode
+            # if they click outside of the box, deactivate typing but do not return what has been typed
+            elif not self.hitbox.collidepoint(mouse_pos) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                self.current_text = ""
+                self.activated = False
+                self.highlighted = False
+            # display the default text if nothing has been entered
+            if self.current_text == "" and not self.activated:
+                self.text = self.default_text
+            else:
+                self.text = self.current_text
+    
+    def reset(self, new_default): # allows the InputBox to be reset or reset with a new default message if needed
+        self.default_text = new_default
+        self.current_text = ""
+
+
+class ProgressBar(): # to display a progress bar that can change during runtime
+    def __init__(self, max_width, colour, outline_colour):
+        self.max_width = max_width
+        self.colour = colour
+        self.outline_colour = outline_colour
+
+    def draw(self, surface, x, y, percentage):
+        pygame.draw.rect(surface, self.outline_colour, (x, y, self.max_width, PROGRESS_BAR_HEIGHT)) # draw outline
+        pygame.draw.rect(surface, self.colour, (x, y, self.max_width*percentage, PROGRESS_BAR_HEIGHT)) # draw progress using the given float value for the percentage
+
+
+class UISquare(): # a single square on the minesweeper board to detect clicks for the minesweeper section
     # does not inherit from any other classes as functionality is quite different
     def __init__(self, position, colour, hover_colour, covered_colour, covered_hover_colour):
         self.position = position
@@ -83,18 +157,18 @@ class UISquare(): # a single square on the minesweeper board to detect clicks fo
         # draw differently depending on whether the square is revealed
         if is_revealed:
             if self.highlighted:
-                pygame.draw.rect(surface, self.hover_colour, self.hitbox, border_radius=CORNER_ROUNDING)
+                pygame.draw.rect(surface, self.hover_colour, self.hitbox)
             else:
-                pygame.draw.rect(surface, self.colour, self.hitbox, border_radius=CORNER_ROUNDING)
+                pygame.draw.rect(surface, self.colour, self.hitbox)
             text = number_font.render(number, True, number_colour)
-            surface.blit(text, (x,y))
+            surface.blit(text, (x+abs((width-text.get_width())//2),y+abs((width-text.get_height())//2)))
         else:
             if self.highlighted:
-                pygame.draw.rect(surface, self.covered_hover_colour, self.hitbox, border_radius=CORNER_ROUNDING)
+                pygame.draw.rect(surface, self.covered_hover_colour, self.hitbox)
             else:
-                pygame.draw.rect(surface, self.covered_colour, self.hitbox, border_radius=CORNER_ROUNDING)
+                pygame.draw.rect(surface, self.covered_colour, self.hitbox)
             if is_flagged:
-                surface.blit(pygame.image.load("flag.png").convert_alpha(), (x,y))
+                surface.blit(pygame.transform.smoothscale(pygame.image.load("flag.png").convert_alpha(), (width, width)), (x,y))
 
     def registerClick(self, event):
         mouse_pos = pygame.mouse.get_pos()
@@ -109,55 +183,3 @@ class UISquare(): # a single square on the minesweeper board to detect clicks fo
                     return 1, self.position
         else:
             self.highlighted = False
-
-class InputBox(Button): # for the user to input text
-    def __init__(self, colour, hover_colour, default_text, text_colour):
-        self.default_text = default_text
-        self.current_text = ""
-        self.activated = False
-        super().__init__(self, colour, hover_colour, default_text, text_colour)
-
-    def update(self, event):
-        mouse_pos = pygame.mouse.get_pos()
-        
-        # activated represents the state where the user can edit the text in the InputBox
-        if not self.activated:
-            self.highlighted = False
-            # once the user clicks on the box it becomes activated and they can enter text
-            if self.hitbox.collidepoint(mouse_pos) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                self.activated = True
-        elif self.activated:
-            self.highlighted = True
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    self.current_text = self.current_text[:-1]
-                # if they click enter, return the text
-                elif event.key == pygame.K_RETURN:
-                    self.activated = False
-                    return self.current_text
-                else:
-                    if event.key not in [pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_LCTRL, pygame.K_RCTRL]:
-                        self.current_text += event.unicode
-            # if they click outside of the box, deactivate typing but do not return what has been typed
-            elif not self.hitbox.collidepoint(mouse_pos) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                self.activated = False
-            # display the default text if nothing has been entered
-            if self.current_text == "":
-                self.text = self.default_text
-            else:
-                self.text = self.current_text
-    
-    def reset(self, new_default): # allows the InputBox to be reset or reset with a new default message if needed
-        self.default_text = new_default
-        self.current_text = ""
-
-class ProgressBar(): # to display a progress bar that can change during runtime
-    def __init__(self, max_width, colour, outline_colour):
-        self.max_width = max_width
-        self.colour = colour
-        self.outline_colour = outline_colour
-
-    def draw(self, surface, x, y, percentage):
-        pygame.draw.rect(surface, self.outline_colour, (x, y, self.max_width, PROGRESS_BAR_HEIGHT)) # draw outline
-        pygame.draw.rect(surface, self.colour, (x, y, self.max_width*percentage, PROGRESS_BAR_HEIGHT)) # draw progress using the given float value for the percentage
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
-import gui_elements as gui
+import gui_elements as gui # import the GUI core element classes from the separate file 
 
 pygame.init()
 
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 900
+
+pygame.key.set_repeat(500, 40) # allow the ability to hold keys like backspace to input them multiple times. 500 and 40 are used as these are the standard millisecond timings for key repeats across many operating systems
 
 # making the colour palette easily accessible through a dictionary and using memorable names
 colours = {"WHITE":"#d9d9d9",
@@ -14,6 +16,15 @@ colours = {"WHITE":"#d9d9d9",
            "LIGHT COAL":"#353a49",
            "DARK COAL":"#2c2f3b",}
 
+label = gui.Label(colours["WHITE"], "test label", colours["GREY"])
+button = gui.Button(colours["WHITE"], colours["LIGHT SILVER"],"test button", colours["GREY"])
+state_button = gui.StateButton(colours["WHITE"], colours["LIGHT SILVER"], ["state 1", "state 2"], colours["GREY"])
+current_state = "state 1"
+input_box = gui.InputBox(colours["WHITE"], colours["LIGHT SILVER"], "test input box", colours["GREY"])
+progress_bar = gui.ProgressBar(300, colours["LIGHT SILVER"], colours["LIGHT COAL"])
+ui_square = gui.UISquare((0,0), colours["DARK COAL"], colours["LIGHT COAL"], colours["DARK SILVER"], colours["LIGHT SILVER"])
+
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Minespeeder")
 
@@ -22,9 +33,36 @@ running = True
 while running:
 
     screen.fill(colours["GREY"])
-    
+
+    label.draw(screen, 100, 100)
+    button.draw(screen, 500, 100)
+    state_button.draw(screen, 100, 250)
+    input_box.draw(screen, 500, 250)
+    progress_bar.draw(screen, 100, 400, 0.67)
+    ui_square.draw(screen, 500, 400, 50, "1", colours["WHITE"], True, True)
+
+       
     # event handler for actions like mouse clicks
     for event in pygame.event.get():
+
+        if button.isClicked(event):
+            print("Button clicked")
+
+        new_state = state_button.updateState(event)
+        if new_state != current_state:
+            print(f"state changed to {new_state}")
+            current_state = new_state
+
+        new_input = input_box.update(event)
+        if new_input != None:
+            print(f"{new_input} entered in input box")
+            input_box.reset(new_input)
+
+        square_input = ui_square.registerClick(event)
+        if square_input != None:
+            print(f"ui square clicked, click type {square_input[0]}, position {square_input[1]}")
+
+
         if event.type == pygame.QUIT:
             running = False
     
